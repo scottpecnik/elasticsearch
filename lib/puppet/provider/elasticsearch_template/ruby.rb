72ca8df1a7610edf22ad009de909a85b48d8348d
Scott Pecnik <srpecnik@gmail.com>
2017-09-14 09:34:20 +1000

    adding some debug code

--- lib/puppet/provider/elasticsearch_template/ruby.rb
+++ lib/puppet/provider/elasticsearch_template/ruby.rb
@@ -2,7 +2,7 @@ require 'puppet/provider/rest'
 
 Puppet::Type.type(:elasticsearch_template).provide(
   :ruby,
-  :parent     => Puppet::Provider::REST
+  :parent => Puppet::Provider::REST
 ) do
 
   def exists?
@@ -22,17 +22,18 @@ Puppet::Type.type(:elasticsearch_template).provide(
                 "_template/#{resource[:name]}",
                 'GET',
                 resource[:ssl])
-    puts "get: #{response.code.to_i}"
     return nil if response.code.to_i != 200
     response.code.to_i
   end
 
   def createTemplate
+    puts Puppet::FileSystem.read(resource[:content])
     response = rest(resource[:servername], resource[:port],
                 "_template/#{resource[:name]}",
                 'PUT',
                 resource[:ssl],
                 Puppet::FileSystem.read(resource[:content]))
+    puts "Put: #{response.code.to_i}"
   end
 
   def deleteTemplate
